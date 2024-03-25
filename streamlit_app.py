161905a4be127bcad837658ba1322880031f1516
Robertgithub7 <164849612+Robertgithub7@users.noreply.github.com>
2024-03-25 02:05:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake import connector
+# from snowflake import connector
 
 
 # Write directly to the app
@@ -13,7 +13,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connections("snowflake")  #changed from connections plural
+cnx = st.connection("snowflake")  #changed from connections plural
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
