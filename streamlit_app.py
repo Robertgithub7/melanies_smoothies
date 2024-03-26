406511f3ff742998da51f5bbd64691f212abef6c
Robertgithub7 <164849612+Robertgithub7@users.noreply.github.com>
2024-03-25 22:40:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+# convert snowpark df to pandas to use loc func
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
 	'Choose up to 5 ingredients:',
