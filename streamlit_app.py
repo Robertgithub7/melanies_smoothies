1554d2df5fd1aa3f3b29dfb6d139c04ff6637a66
Robertgithub7 <164849612+Robertgithub7@users.noreply.github.com>
2024-03-25 18:23:58 -0700

    Update streamlit_app.py
    added back st

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = connections("snowflake")  #changed from connections plural - remove "st"
+cnx = st.connections("snowflake") 
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
